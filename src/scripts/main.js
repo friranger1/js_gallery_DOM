6d8b547b629e7801cd0b6c04491eed2fdee2786b
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-03 00:10:42 +0200

    Implemented functional with changing main images after clieck on the one of the thumb

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const bigImg = document.getElementById('largeImg');
+const links = [...document.querySelectorAll('.list-item__link')];
+
+for (const link of links) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    bigImg.src = link.href;
+  });
+}
