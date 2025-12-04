dcab72307c5c67570545ac71c8582d432ed8d376
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-04 14:29:03 +0200

    It was add one chect if link is empty, fixed access to attribute and changed constant link to more accurate value

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,16 @@
 
 const bigImg = document.getElementById('largeImg');
 const thumbs = document.getElementById('thumbs');
-bigImg.src = thumbs.children[0].children[0].attributes[0].nodeValue;
 
-thumbs.addEventListener('click',(e) => {
-  const link = e.target.closest('.gallery__img');
+bigImg.src = thumbs.querySelector('a').href;
+
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('.gallery__thumb');
+
+  if(!link) {
+    return;
+  }
 
   e.preventDefault();
   bigImg.src = link.src;
